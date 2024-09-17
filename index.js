585f0489a5da412cb1183d25dd1940a335542627
sanyadrian <adrianovoleksandr@gmail.com>
2024-09-16 22:34:03 -0400

    Added function for email validation

--- index.js
+++ index.js
@@ -3,13 +3,25 @@ const container1 = document.querySelector('.newsletter-cta')
 const container2 = document.querySelector('.newsletter-cta-2')
 const dismiss = document.querySelector('.dismiss')
 const emailInput = document.querySelector('#email')
+const errorSpan = document.querySelector('.error')
 const successMessageEmail = document.querySelector('.newsletter-cta-2 p strong')
+function isValidEmail (email) {
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  return emailPattern.test(email)
+}
 form.addEventListener('submit', (e) => {
   e.preventDefault()
-  const email = emailInput.value
-  successMessageEmail.textContent = email
-  container1.classList.add('hide')
-  container2.classList.remove('hide')
+  const email = emailInput.value.trim()
+  if (!email || !isValidEmail(email)) {
+    errorSpan.textContent = 'Email address required'
+    errorSpan.style.display = 'block'
+    emailInput.classList.add('invalid')
+  } else {
+    successMessageEmail.textContent = email
+    errorSpan.style.display = 'none'
+    container1.classList.add('hide')
+    container2.classList.remove('hide')
+  }
 })
 dismiss.addEventListener('click', (e) => {
   container2.classList.add('hide')
